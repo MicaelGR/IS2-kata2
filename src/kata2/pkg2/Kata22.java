73833ac89d966ce49f89635bf95fb7cf11cf6a90
Micael <Entrar@PC-1241>
2021-10-08 13:54:11 +0100

    Kata 2 - Version 2

--- src/kata2/pkg2/Kata22.java
+++ src/kata2/pkg2/Kata22.java
@@ -11,12 +11,8 @@ public class Kata22 {
         
         Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         
-        for(int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i])+1);                            
-            } else {
-                histogram.put(data[i], 1);     
-            }
+        for(Integer i : data) {
+            histogram.put(i, histogram.containsKey(i) ? histogram.get(i) + 1 : 1);
         }
         
         for (Integer key : histogram.keySet()) {
